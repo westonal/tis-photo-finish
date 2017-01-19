24fe0a4ca8fc7d6dc4eeed22beb034b9a42ad040
Alan Evans <thealanevans@googlemail.com>
2017-01-19 19:59:34 -0400

    More tokenizer tests

--- src/test/java/com/coltsoftware/tis100/TokenizerTests.kt
+++ src/test/java/com/coltsoftware/tis100/TokenizerTests.kt
@@ -1,18 +1,33 @@
 package com.coltsoftware.tis100
 
-import org.junit.Assert
+import org.hamcrest.Matchers.`is`
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 class TokenizerTests {
 
     @Test
     fun empty() {
-        Assert.assertThat(Tokenizer().tokens(""), org.hamcrest.core.IsEqual<Any>(emptyList<Any>()))
+        assertThat(Tokenizer().tokens(""), `is`(emptyList<TS100Token>()))
+    }
+
+    @Test
+    fun single_token() {
+        assertThat(Tokenizer().tokens("MOV"), `is`(listOf(TS100Token(tokenString = "MOV", position = 0))))
+    }
+
+    @Test
+    fun different_single_token() {
+        assertThat(Tokenizer().tokens("ADD"), `is`(listOf(TS100Token(tokenString = "ADD", position = 0))))
     }
 }
 
+data class TS100Token(val tokenString: String, val position: Int)
+
 class Tokenizer {
-    fun tokens(s: String): List<Any> {
-        return emptyList()
+    fun tokens(s: String): List<TS100Token> {
+        if (s.isEmpty())
+            return emptyList()
+        return listOf(TS100Token(tokenString = s, position = 0))
     }
 }
