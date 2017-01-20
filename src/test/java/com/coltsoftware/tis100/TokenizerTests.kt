882da538c88fc0edd2f83ee52cc95f094caa94f4
Alan Evans <thealanevans@googlemail.com>
2017-01-19 20:10:52 -0400

    More tokenizer tests

--- src/test/java/com/coltsoftware/tis100/TokenizerTests.kt
+++ src/test/java/com/coltsoftware/tis100/TokenizerTests.kt
@@ -13,12 +13,30 @@ class TokenizerTests {
 
     @Test
     fun single_token() {
-        assertThat(Tokenizer().tokens("MOV"), `is`(listOf(TS100Token(tokenString = "MOV", position = 0))))
+        assertThat(Tokenizer().tokens("MOV"),
+                `is`(listOf(TS100Token(tokenString = "MOV", position = 0))))
     }
 
     @Test
     fun different_single_token() {
-        assertThat(Tokenizer().tokens("ADD"), `is`(listOf(TS100Token(tokenString = "ADD", position = 0))))
+        assertThat(Tokenizer().tokens("ADD"),
+                `is`(listOf(TS100Token(tokenString = "ADD", position = 0))))
+    }
+
+    @Test
+    fun two_tokens() {
+        assertThat(Tokenizer().tokens("ADD LEFT"),
+                `is`(listOf(TS100Token(tokenString = "ADD", position = 0),
+                        TS100Token(tokenString = "LEFT", position = 4)
+                )))
+    }
+
+    @Test
+    fun two_tokens_more_spaces() {
+        assertThat(Tokenizer().tokens("  ADD   LEFT "),
+                `is`(listOf(TS100Token(tokenString = "ADD", position = 2),
+                        TS100Token(tokenString = "LEFT", position = 8)
+                )))
     }
 }
 
@@ -28,6 +46,27 @@ class Tokenizer {
     fun tokens(s: String): List<TS100Token> {
         if (s.isEmpty())
             return emptyList()
-        return listOf(TS100Token(tokenString = s, position = 0))
+        val list = mutableListOf<TS100Token>()
+        val currentToken = StringBuilder()
+        var currentTokenStart = 0
+        var index = 0
+        s.forEach {
+            if (it === ' ') {
+                if (!currentToken.isEmpty()) {
+                    list.add(TS100Token(tokenString = currentToken.toString(),
+                            position = currentTokenStart))
+                    currentToken.delete(0, currentToken.length)
+                }
+            } else {
+                if (currentToken.isEmpty())
+                    currentTokenStart = index
+                currentToken.append(it)
+            }
+            index++
+        }
+        if (!currentToken.isEmpty())
+            list.add(TS100Token(tokenString = currentToken.toString(),
+                    position = currentTokenStart))
+        return list
     }
 }
